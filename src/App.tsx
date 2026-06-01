690e4e3ddf3342706084c0bb486caee4a5249895
Fuyula <fuyula@pm.me>
2026-06-01 18:39:40 +0200

    feat: create Vite project, add styling libraries

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main
+      aria-label='Security events'
+      className='flex flex-col justify-center items-center min-h-screen'
+    >
+      <h1 className=' text-2xl lg:text-3xl font-extrabold'>Security events</h1>
+      <p>Work in progress</p>
+    </main>
+  );
+}
+
+export default App;
